f9faebce507cecdaaaeacece159a4bd9f63abaa6
reinhartcjp <81622295+reinhartcjp@users.noreply.github.com>
2021-09-06 13:16:03 +0700

    Add files via upload

---
+++ data/ews_2.js
@@ -0,0 +1,15 @@
+var json_ews_2 = {
+"type": "FeatureCollection",
+"name": "ews_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "EWS 6", "link": "<img src=\"https:\/\/i.ibb.co\/dkrDcSx\/Meteseh.jpg\" alt=\"Meteseh\" border=\"0\" width=\"350\" height=\"200\">" }, "geometry": { "type": "Point", "coordinates": [ 110.4687014, -7.0650022, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "EWS 1", "link": "<img src=\"https:\/\/i.ibb.co\/2yY6czp\/Ngaliyan-4.jpg\" alt=\"Ngaliyan-4\" border=\"0\" width=\"350\" height=\"200\">" }, "geometry": { "type": "Point", "coordinates": [ 110.3073343, -6.9710085, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "EWS 2", "link": "<img src=\"https:\/\/i.ibb.co\/PMcg6wR\/Ngaliyan-3.jpg\" alt=\"Ngaliyan-3\" border=\"0\" width=\"350\" height=\"200\">" }, "geometry": { "type": "Point", "coordinates": [ 110.3055815, -6.9754582, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "EWS 3", "link": "<img src=\"https:\/\/i.ibb.co\/D7gbyMy\/Ngaliyan-Mts.jpg\" alt=\"Ngaliyan-Mts\" border=\"0\" width=\"350\" height=\"200\">" }, "geometry": { "type": "Point", "coordinates": [ 110.3194518, -7.0059748, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "EWS 4", "link": "<img src=\"https:\/\/i.ibb.co\/zh9TRTK\/Ngaliyan-Jbt.jpg\" alt=\"Ngaliyan-Jbt\" border=\"0\" width=\"350\" height=\"200\">" }, "geometry": { "type": "Point", "coordinates": [ 110.3429016, -6.9972361, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "EWS 5", "link": "<img src=\"https:\/\/i.ibb.co\/ggYNQxJ\/Genuk.jpg\" alt=\"Genuk\" border=\"0\" width=\"350\" height=\"200\">" }, "geometry": { "type": "Point", "coordinates": [ 110.4963357, -6.9686928, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "EWS 7", "link": "<img src=\"https:\/\/i.ibb.co\/9TgQYcy\/Puncang-Gading-2.jpg\" alt=\"Puncang-Gading-2\" border=\"0\" width=\"350\" height=\"200\">" }, "geometry": { "type": "Point", "coordinates": [ 110.4843491, -7.0404592, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Bendungan Pucang Gading 2", "link": "<img src=\"https:\/\/i.ibb.co\/hHTbHmc\/Puncang-Gading.jpg\" alt=\"Puncang-Gading\" border=\"0\" width=\"350\" height=\"200\">" }, "geometry": { "type": "Point", "coordinates": [ 110.484251, -7.0398719, 0.0 ] } }
+]
+}
